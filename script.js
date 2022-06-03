e0f9a20656bf3e1a485ebb493c4be4d944626829
President <aaron.m.wilson@uconn.edu>
2022-06-03 19:31:28 -0400

    added initial prompt as well as following prompts

--- script.js
+++ script.js
@@ -11,12 +11,34 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+  enter = parseInt(prompt("what length would you like your password to be bb girl?"));
+
+  if (enter <8 || enter > 128) {
+
+    enter = parseInt(prompt("You must choose between 8 and 128"));
+  } else {
+    confirmNumber = confirm("Will this contain numbers?");
+        confirmCharacter = confirm("Will this contain special characters?");
+        confirmUppercase = confirm("Will this contain Uppercase letters?");
+        confirmLowercase = confirm("Will this contain Lowercase letters?");
+    };
+  
+
+  
+
+    
+  
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+ 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
