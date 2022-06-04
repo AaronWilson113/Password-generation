699a21faaf9faaaf736a5e5bfb6f8282769243aa
President <aaron.m.wilson@uconn.edu>
2022-06-04 06:18:29 -0400

    added initial variables again as well as arrays for future function variables

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment Code
 
+var enter; 
+var confirmNum
+var confirmChar
+var confirmUpper
+var confirmLower
+
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 //Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +23,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() 
-
-
+ 
 
 
 
