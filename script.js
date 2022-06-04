286691fa2e641519651e06e606d874fb49c1faff
President <aaron.m.wilson@uconn.edu>
2022-06-03 21:44:44 -0400

    changed letter, symbol, and number variables into arrays

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 
-var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; //AKA lowercase. need to add uppercase
-var numbers = "0123456789";
-var symbols = "!@#$%^&*_-+=";
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //AKA lowercase. need to add uppercase
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 
 
@@ -112,12 +112,12 @@ function writePassword() {
      else if (logLowercase) {
 
       choice = letters;
-      
+
      }
     
 
 
-
+  
 
 
 
